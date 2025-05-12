6ac9623ea9f30298b9131aac51bdb4e7b604cab6
BROCK-HOME\brock <brockqq@gmail.com>
2025-05-12 08:36:22 +0800

    feat 增加 cucumber

---
+++ src/test/com/java/com/example/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.example.stepdefs")
+public class RunCucumberTest {
+}
